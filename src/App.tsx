bca511b67f1e56792c4e6534bd1440d9cffebaf4
haoping6028 <lucas60303@gmail.com>
2025-01-07 11:04:38 +0800

    Modify the main page introdunction

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 // import React from 'react';
-import AnimatedBox from './components/AnimatedBox';
+// import AnimatedBox from './components/AnimatedBox';
 import PopOutBox from './components/PopOutBox';
 // import StaggeredPopOut from './components/StaggeredPopOut';
 
 function App() {
   return (
     <div className="App">
-      <h1>GSAP with Vite, React, and TypeScript</h1>
-      <AnimatedBox />
+      <h1>歡迎來到我的網站</h1>
+      <h2>網站目前測試中👨‍🔧</h2>
       <PopOutBox />
-      {/* <StaggeredPopOut /> */}
     </div>
   );
 }
