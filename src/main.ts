8d3dee088157e7b2923951a6c1fb98aacb602fef
haoping6028 <lucas60303@gmail.com>
2025-01-07 18:36:02 +0800

    Redeploy github actions

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import './style.css';
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>歡迎來到我的網站</h1>
-    <h2>目前網站測試中 🔥</h2>
+    <h2>目前網站測試中🔥</h2>
     <footer>Hao-Ping Chen © 2025</footer>
   </div>
 `;
