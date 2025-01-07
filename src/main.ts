e989cebc7a5c68037e90b255f796183015d8e4c8
haoping6028 <lucas60303@gmail.com>
2025-01-07 18:19:59 +0800

    Change framework from React to Vanilla TS

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css';
+// import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>歡迎來到我的網站</h1>
+    <h2>目前網站測試中🔥</h2>
+  </div>
+`;
+
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
