1baa5963bed6b32b6682ba0a08688a392718eed6
haoping6028 <lucas60303@gmail.com>
2025-01-08 21:32:16 +0800

    Declare the type of maath random function

---
+++ src/types/maath-random.d.ts
@@ -0,0 +1,7 @@
+declare module "maath/random/dist/maath-random.esm.js" {
+  export function inSphere(
+    array: Float32Array,
+    options?: { radius?: number }
+  ): Float32Array;
+  // ...declare other functions if needed...
+}
