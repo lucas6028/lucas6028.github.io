f43c835ca5aebc25434a5e853fd3de2a077194da
haoping6028 <lucas60303@gmail.com>
2025-01-08 15:54:57 +0800

    Complete about and SectionWrapper components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,55 @@
-const About = () => {
+import { Tilt } from "react-tilt"
+import { motion } from "framer-motion"
+
+import { styles } from "../style"
+import { services } from "../constant"
+import { fadeIn, textVariant } from "../utils/motion"
+import { SectionWrapper } from "../hoc"
+
+interface ServiceCardProps {
+  index: number;
+  title: string;
+  icon: string;
+}
+
+const ServiceCard: React.FC<ServiceCardProps> = ({ index, title, icon }) => {
+  // console.log(title + " " + icon)
   return (
-    <div>About</div>
+    <Tilt className="xs:w-[250px] w-full" options={{ max: 45, scale: 1, speed: 450 }}>
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+        <div className="bg-tertiary p-6 rounded-[20px] py-5 px-12 
+          min-h-[280px] flex justify-center items-center flex-col">
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px]
+        max-w-3xl leading[30px]">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Ut accusantium quibusdam veniam ratione quasi magnam minus consectetur,
+        recusandae asperiores amet eius quidem.
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard title={service.title} index={index} icon={service.icon} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+const WrappedAbout = SectionWrapper(About, "about");
+export default WrappedAbout;
---
+++ src/hoc/SectionWrapper.tsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { staggerContainer } from "../utils/motion";
+import { FunctionComponent } from "react";
+
+// interface SectionWrapperProps {
+//     idName: string;
+// }
+
+const SectionWrapper = (Component: FunctionComponent, idName: string) =>
+    function HOC() {
+        return (
+            <motion.section
+                variants={staggerContainer({ staggerChildren: 0.1 })}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: true, amount: 0.25 }}
+                className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+            >
+                <span className="hash-span" id={idName}>
+                    &nbsp;
+                </span>
+                <Component />
+            </motion.section>
+        );
+    };
+
+export default SectionWrapper;
