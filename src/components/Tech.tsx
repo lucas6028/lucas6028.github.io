5f24704de7543ee46cc36ad9b8414afe17f6ec30
haoping6028 <lucas60303@gmail.com>
2025-01-08 17:37:26 +0800

    Complete technologies balls

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,7 +1,18 @@
+import { BallCanvas } from "./canvas"
+import { SectionWrapper } from "../hoc"
+import { technologies } from "../constant"
+
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((tech) => (
+        <div key={tech.name} className="w-28 h-28">
+          <BallCanvas icon={tech.icon} />
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+const TechSection = SectionWrapper(Tech, "tech")
+export default TechSection
