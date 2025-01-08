7bf327dc0accd1e884b733bc9175ece69e105211
haoping6028 <lucas60303@gmail.com>
2025-01-08 21:33:14 +0800

    Complete contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,116 @@
+import { useState, useRef } from "react"
+import { motion } from "framer-motion"
+import emailJs from "@emailjs/browser"
+
+import { styles } from "../style"
+import { EarthCanvas } from "./canvas"
+import { SectionWrapper } from "../hoc"
+import { slideIn } from "../utils/motion"
+
 const Contact = () => {
+  const formRef = useRef<HTMLFormElement>(null)
+  const [loading, setLoading] = useState(false)
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setLoading(true)
+
+    emailJs.send(
+      import.meta.env.VITE_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Lucas",
+        from_email: form.email,
+        to_email: import.meta.env.VITE_EMAILJS_USER_ID,
+        message: form.message,
+      },
+      import.meta.env.VITE_EMAILJS_USER_ID
+    ).then(() => {
+      setLoading(false)
+      alert("Thank you. I will get back to you soon.")
+      setForm({
+        name: "",
+        email: "",
+        message: "",
+      })
+    }
+    ).catch(() => {
+      setLoading(false)
+      alert("Ahh, something went wrong. Please try again.")
+    })
+  }
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflwo-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col gap-8">
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="bg-tertiary py-4 px-6 rounded-lg placeholder:text-secondary 
+              text-white outline-none bordr-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className="bg-tertiary py-4 px-6 rounded-lg placeholder:text-secondary 
+              text-white outline-none bordr-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+              rows={7}
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className="bg-tertiary py-4 px-6 rounded-lg placeholder:text-secondary 
+              text-white outline-none bordr-none font-medium"
+            />
+          </label>
+          <button
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none w-fit text-white
+             font-bold shadow-md shadow-primary rounded-xl">
+            {loading ? "Sending..." : "Send"}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
+        <EarthCanvas />
+      </motion.div>
+    </div>
   )
 }
 
-export default Contact
+const ContactWrapper = SectionWrapper(Contact, "contact")
+export default ContactWrapper
+
