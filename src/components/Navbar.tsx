31113665cfc888a78df844fe98e49743c648f918
haoping6028 <lucas60303@gmail.com>
2025-01-09 11:45:07 +0800

    Modify the url of navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = () => {
               onClick={() => {
                 setActive(link.title);
               }}>
-              <a href={`#:${link.id}`}>{link.title}</a>
+              <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
         </ul>
