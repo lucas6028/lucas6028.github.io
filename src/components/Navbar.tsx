b0909c8f79762de003593e89053e80e1e8d566e7
haoping6028 <lucas60303@gmail.com>
2025-01-10 10:28:30 +0800

    Fix navbar href links error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -48,7 +48,7 @@ const Navbar = () => {
                     setToggle(!toggle);
                     setActive(link.title);
                   }}>
-                  <a href={`#:${link.id}`}>{link.title}</a>
+                  <a href={`#${link.id}`}>{link.title}</a>
                 </li>
               ))}
             </ul>
