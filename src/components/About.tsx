00ded6b89cba7335f51193d59576aec5ee95baea
haoping6028 <lucas60303@gmail.com>
2025-01-10 10:42:00 +0800

    Modify the overview of about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -38,9 +38,9 @@ const About = () => {
       </motion.div>
       <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px]
         max-w-3xl leading-[30px]">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        Ut accusantium quibusdam veniam ratione quasi magnam minus consectetur,
-        recusandae asperiores amet eius quidem.
+        I'm a skilled software developer with experience in TypeScript and JavaScript, and expertise in frameworks like React and Node.js.
+        I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems.
+        Let's work together to bring your ideas to life!
       </motion.p>
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
